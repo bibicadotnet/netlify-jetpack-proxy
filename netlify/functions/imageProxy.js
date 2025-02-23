271cdfea17058140c61db79b78226317f5755d38
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-02-23 23:16:00 +0700

    Update imageProxy.js

--- netlify/functions/imageProxy.js
+++ netlify/functions/imageProxy.js
@@ -1,66 +1,46 @@
 // netlify/functions/imageProxy.js
+const CACHE_DURATION = '31536000'; // 1 year in seconds
 
 exports.handler = async (event, context) => {
-  const url = new URL(event.rawUrl);
-  
-  const rules = {
-    '/avatar': {
-      targetHost: 'secure.gravatar.com',
-      pathTransform: (path, prefix) => '/avatar' + path.replace(prefix, ''),
-      service: 'Gravatar'
-    },
-    '/comment': {
-      targetHost: 'i0.wp.com',
-      pathTransform: (path, prefix) => '/comment.bibica.net/static/images' + path.replace(prefix, ''),
-      service: 'Artalk & Jetpack'
-    },
-    '/': {
-      targetHost: 'i0.wp.com',
-      pathTransform: (path) => '/bibica.net/wp-content/uploads' + path,
-      service: 'Jetpack'
-    }
-  };
-
-  const rule = Object.entries(rules).find(([prefix]) => url.pathname.startsWith(prefix));
-  
-  if (!rule) {
-    return {
-      statusCode: 404,
-      body: `Path not supported: ${url.pathname}`
-    };
-  }
-
-  const targetUrl = new URL(event.rawUrl);
-  const [prefix, config] = rule;
-  
-  targetUrl.hostname = config.targetHost;
-  targetUrl.pathname = config.pathTransform(url.pathname, prefix);
-  targetUrl.search = url.search;
-
   try {
-    const response = await fetch(targetUrl, {
-      headers: {
-        'Accept': event.headers.accept || '*/*'
-      }
-    });
+    const url = new URL(event.rawUrl);
+    const path = decodeURIComponent(url.pathname);
+    
+    let targetUrl = new URL('https://i0.wp.com');
+    let service = 'Jetpack';
+    
+    if (path.startsWith('/avatar')) {
+      targetUrl.hostname = 'secure.gravatar.com';
+      targetUrl.pathname = '/avatar' + path.replace('/avatar', '');
+      service = 'Gravatar';
+    } else if (path.startsWith('/comment')) {
+      targetUrl.hostname = 'i0.wp.com';
+      targetUrl.pathname = '/comment.bibica.net/static/images' + path.replace('/comment', '');
+      service = 'Artalk & Jetpack';
+    } else {
+      targetUrl.pathname = '/bibica.net/wp-content/uploads' + path;
+    }
+    
+    targetUrl.search = url.search;
 
+    const response = await fetch(targetUrl.toString());
     const body = await response.arrayBuffer();
 
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'image/webp',
-        'Link': response.headers.get('link'),
-        'X-Cache': response.headers.get('x-nc'),
-        'X-Served-By': `Netlify Functions & ${config.service}`
+        'Cache-Control': `public, max-age=${CACHE_DURATION}`,
+        'X-Served-By': `Netlify Functions & ${service}`
       },
       body: Buffer.from(body).toString('base64'),
       isBase64Encoded: true
     };
+
   } catch (error) {
     return {
       statusCode: 500,
-      body: `Error fetching image: ${error.message}`
+      body: JSON.stringify({ error: 'Failed to proxy image' })
     };
   }
 };
