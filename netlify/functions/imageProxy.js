4b46c0c547a5cc6c3b92700a164b8f6821bcf4d3
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-02-24 11:28:19 +0700

    Update imageProxy.js

--- netlify/functions/imageProxy.js
+++ netlify/functions/imageProxy.js
@@ -1,49 +1,34 @@
-// netlify/functions/imageProxy.js
+// netlify/edge-functions/imageProxy.js
 
-exports.handler = async (event, context) => {
-  try {
-    const url = new URL(event.rawUrl);
-    const path = decodeURIComponent(url.pathname);
-    
-    let targetUrl = new URL('https://i0.wp.com');
-    let service = 'Jetpack';
-    
-    if (path.startsWith('/avatar')) {
-      targetUrl.hostname = 'secure.gravatar.com';
-      targetUrl.pathname = '/avatar' + path.replace('/avatar', '');
-      service = 'Gravatar';
-    } else if (path.startsWith('/comment')) {
-      targetUrl.hostname = 'i0.wp.com';
-      targetUrl.pathname = '/comment.bibica.net/static/images' + path.replace('/comment', '');
-      service = 'Artalk & Jetpack';
-    } else {
-      targetUrl.pathname = '/bibica.net/wp-content/uploads' + path;
-    }
-    
-    targetUrl.search = url.search;
-
-    const response = await fetch(targetUrl.toString());
-    const body = await response.arrayBuffer();
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'image/webp',
-        'Cache-Control': 'public, max-age=31536000, s-maxage=31536000, immutable',
-        'X-Served-By': `Netlify Functions & ${service}`,
-        'Vary': 'Accept'
-      },
-      body: Buffer.from(body).toString('base64'),
-      isBase64Encoded: true
-    };
-
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to proxy image' }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
+export default async function handler(request, context) {
+  const url = new URL(request.url);
+  const path = decodeURIComponent(url.pathname);
+  
+  let targetUrl = new URL('https://i0.wp.com');
+  let service = 'Jetpack';
+  
+  if (path.startsWith('/avatar')) {
+    targetUrl.hostname = 'secure.gravatar.com';
+    targetUrl.pathname = '/avatar' + path.replace('/avatar', '');
+    service = 'Gravatar';
+  } else if (path.startsWith('/comment')) {
+    targetUrl.hostname = 'i0.wp.com';
+    targetUrl.pathname = '/comment.bibica.net/static/images' + path.replace('/comment', '');
+    service = 'Artalk & Jetpack';
+  } else {
+    targetUrl.pathname = '/bibica.net/wp-content/uploads' + path;
   }
-};
+  
+  targetUrl.search = url.search;
+
+  const response = await fetch(targetUrl.toString());
+  const body = await response.arrayBuffer();
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'image/webp',
+      'Cache-Control': 'public, max-age=31536000',
+      'X-Served-By': `Netlify Functions & ${service}`
+    }
+  });
+}
